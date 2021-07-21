e046b72a825fae09a0e7fa641a5d983c4c737c76
Troza <trozah@gmail.com>
2021-07-21 17:02:18 -0500

    Delete started

--- bookmanager.py
+++ bookmanager.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -18,14 +18,39 @@ class Book(db.Model):
 	def __repr__(self):
 		return '<Title: {}>'.format(self.title)
 
-@app.route('/', methods=['GET', "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def home():
 	if request.form:
-		book = Book(title=request.form.get('title'))
-		db.session.add(book)
-		db.session.commit()
+		try:
+			book = Book(title=request.form.get('title'))
+			db.session.add(book)
+			db.session.commit()
+		except Exception as e:
+			print("Failed to add book")
+			print(e)
 	books = Book.query.all()
-	return render_template('home.html')
+	return render_template('home.html', books=books)
+
+@app.route('/update', methods=["POST"])
+def update():
+	try:
+		newtitle = request.form.get('newtitle')
+		oldtitle = request.form.get('oldtitle')
+		book = Book.query.filter_by(title=oldtitle).first()
+		book.title = newtitle
+		db.session.commit()
+	except Exception as e:
+		print("Couldn't update book title")
+		print(e)
+	return redirect('/')
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    title = request.form.get("title")
+    book = Book.query.filter_by(title=title).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == '__main__':
 	app.run(host='localhost', debug=True)
